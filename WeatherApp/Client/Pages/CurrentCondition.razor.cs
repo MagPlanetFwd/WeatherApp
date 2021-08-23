66c9f6e93e4c101cb605e99c57cd539feb157c09
Matuts Jiraboondilok <matuts.j@gmail.com>
2021-08-22 21:52:53 -0700

    add current conditions

---
+++ WeatherApp/Client/Pages/CurrentCondition.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WeatherApp.Shared;
+
+namespace WeatherApp.Client.Pages
+{
+    public class CurrentConditionBase : ComponentBase
+    {
+        [Inject]
+        public HttpClient Client { get; set; }
+
+        protected CurrentWeather Current { get; private set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Current = await Client.GetFromJsonAsync<CurrentWeather>("Current");
+        }
+    }
+}
