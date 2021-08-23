9c22a560f665d43c0a495e785e8cb3f39ce0511c
Matuts Jiraboondilok <matuts.j@gmail.com>
2021-08-22 21:34:21 -0700

    add current temp

--- WeatherApp/Client/Pages/CurrentTemp.razor.cs
+++ WeatherApp/Client/Pages/CurrentTemp.razor.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+using WeatherApp.Shared;
 
 namespace WeatherApp.Client.Pages
 {
     public class CurrentTempBase : ComponentBase
     {
+        [Inject]
+        public HttpClient Client { get; set; }
 
+        protected CurrentWeather Current { get; private set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Current = await Client.GetFromJsonAsync<CurrentWeather>("Current");
+        }
     }
 }
